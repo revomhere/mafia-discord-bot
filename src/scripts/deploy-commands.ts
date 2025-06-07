5e5bc5bd2401a1542deabe05f1ce7c602aca7607
Nazar Shevchenko <shevanzr@gmail.com>
2025-06-07 21:14:32 +0300

    deploy commands fix

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -3,12 +3,12 @@ import '@/setup';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord.js';
 import { getCommands } from '@/helpers';
+import config from '@/config';
+
+const { appId, guildId } = config;
 
 const rest = new REST({ version: '10' }).setToken(process.env.PRIVATE_TOKEN || '');
 const commands = await getCommands('@/commands');
-
-const appId = process.env.APP_ID || '';
-const guildId = process.env.GUILD_ID || '';
 
 await rest.put(Routes.applicationCommands(appId), { body: [] });
 
@@ -16,4 +16,4 @@ await rest.put(Routes.applicationGuildCommands(appId, guildId), {
   body: commands.map(command => command.data.toJSON())
 });
 
-console.log(`Successfully registered ${commands.length} application commands.`);
+console.log(`Successfully registered ${commands.length} application commands to ${guildId}`);
